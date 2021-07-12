94316fe1df6c325507eaa3d7c52f2c6af878734e
Tomasz Gołkowski <toxdg@o2.pl>
2021-07-12 11:09:36 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,1 +1,1 @@
-npm install --save-dev --no-audit run-sequence gulp fs gulp-watch gulp-sourcemaps gulp-autoprefixer gulp-sass sass gulp-concat gulp-minify gulp-util autoprefixer vinyl-ftp
+npm install --save-dev --no-audit run-sequence gulp fs gulp-uglify gulp-watch gulp-sourcemaps gulp-autoprefixer gulp-sass sass gulp-concat gulp-minify gulp-util autoprefixer vinyl-ftp
